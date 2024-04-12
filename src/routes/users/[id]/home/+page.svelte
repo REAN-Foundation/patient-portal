53eb46dfae8d880bfa5a9376689a94e2d9b8a94c
Dattatraya-Londhe <londhedn@gmail.com>
2024-04-12 15:19:27 +0530

    Fixed color theme

--- src/routes/users/[id]/home/+page.svelte
+++ src/routes/users/[id]/home/+page.svelte
@@ -100,23 +100,23 @@
 	};
 </script>
 <Modal background="bg-white text-black "/>
-<h1>This is home page of the user</h1>
+<!-- <h1>This is home page of the user</h1>
 <h1>{JSON.stringify(data.user)}</h1>
 <button on:click={handleDelete}>Delete Account</button>
-<button on:click={handleLogout}>Logout</button>
-
+<button on:click={handleLogout}>Logout</button> -->
+<div class="bg-[#F3F4F6]">
 <div class="absolute top-0 right-0 mt-2 mr-2">
-    <button class="bg-[#7165E3] text-white px-4 py-2 rounded-lg">Sign Out</button>
-    <button class="bg-[#7165E3] text-white px-4 py-2 rounded-lg ml-2">Delete Account</button>
+    <button on:click={handleLogout} class="variant-filled-secondary text-white px-4 py-2 rounded-lg">Sign Out</button>
+    <button on:click={handleDelete} class="variant-filled-secondary text-white px-4 py-2 rounded-lg ml-2">Delete Account</button>
   </div>
 <div class="flex justify-center items-center min-h-screen mx-8">
     <div class="relative w-full md:max-w-md bg-white rounded-xl overflow-hidden shadow-lg">
-      <div class="bg-[#7165E3] text-white py-4 text-center text-xl">
+      <div class="variant-filled-secondary text-white py-4 text-center text-xl">
         Welcome User!
       </div>
       <div class="p-8">
-        <p class="text-lg">
-          <span class="font-semibold">First Name:</span> {data.user.FirstName}<br>
+        <p class="text-lg text-black">
+          <span class="font-semibold ">First Name:</span> {data.user.FirstName}<br>
           <span class="font-semibold">Middle Name:</span> {data.user.MiddleName}<br>
           <span class="font-semibold">Last Name:</span> {data.user.LastName}<br>
           <span class="font-semibold">Gender:</span> {data.user.Gender}<br>
@@ -126,3 +126,4 @@
       </div>
     </div>
   </div>
+</div>
