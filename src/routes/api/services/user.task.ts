5cd1781a1e57544f4f3e462145ee74d5468c1abf
Rupali Dinde <56864828+rupali-inflection@users.noreply.github.com>
2025-02-19 18:16:41 +0530

    Fixed multiple careplan issue (#32)

--- src/routes/api/services/user.task.ts
+++ src/routes/api/services/user.task.ts
@@ -2,6 +2,7 @@ import { BACKEND_API_URL } from "$env/static/private";
 import { CacheService } from "$lib/server/cache/cache.service";
 import { Helper } from "$lib/utils.ts/helper";
 import { DateStringFormat } from "$lib/utils.ts/time.types";
+import { error } from "@sveltejs/kit";
 import { get_ } from "./common";
 
 /////////////////////////////////////////////////////////////////////////////
@@ -23,6 +24,17 @@ export const getUserTasks = async (sessionId: string, searchParams?: any) => {
 		}
 	}
 	const url = BACKEND_API_URL + `/user-tasks/search${searchString}`;
+	const result = await get_(url, true, sessionId);
+    return result;
+};
+
+export const getEnrollments = async (sessionId: string, patientUserId: string) => {
+	const url = BACKEND_API_URL + `/care-plans/patients/${patientUserId}/enrollments`;
+	return await get_(url, true, sessionId);
+};
+
+export const getCarePlanTasks = async (sessionId: string, userId: string) => {
+    let itemsPerPage = 25;
 	const today = Helper.getDateString(new Date(), DateStringFormat.YYYY_MM_DD);
 	const cacheKey = `session-${sessionId}:req-getUserTasks:${today}`;
 	const yesterday = Helper.getYesterdayDate();
@@ -32,15 +44,32 @@ export const getUserTasks = async (sessionId: string, searchParams?: any) => {
         await CacheService._cache.delete(yesterdayCacheKey);
         console.log(`Cleared old key: ${yesterdayCacheKey}`);
     }
-    if (await CacheService.has(cacheKey)) {
-        return await CacheService.get(cacheKey);
+ 
+    let searchParams = {
+        userId: userId,
+        ActionType: 'CarePlan',
+        itemsPerPage: itemsPerPage
+    };
+
+    let response = await getUserTasks(sessionId, searchParams);
+
+    if (response.Status === 'failure' || response.HttpCode !== 200) {
+        throw error(response.HttpCode, response.Message || 'An error occurred');
     }
-	const result = await get_(url, true, sessionId);
-	await CacheService.set(cacheKey, result);
-    return result;
+
+    console.log('response-----', response.Data.UserTasks);
+
+    let userTasks = response.Data.UserTasks.Items;
+    let totalCount = response.Data.UserTasks.TotalCount;
+    if (totalCount > itemsPerPage) {
+        searchParams.itemsPerPage = totalCount;
+        response = await getUserTasks(sessionId, searchParams);
+        userTasks = response.Data.UserTasks.Items;
+    }
+
+    await CacheService.set(cacheKey, userTasks);
+    console.log(`Cached data with key: ${cacheKey}`);
+
+    return userTasks;
 };
 
-export const getEnrollments = async (sessionId: string, patientUserId: string) => {
-	const url = BACKEND_API_URL + `/care-plans/patients/${patientUserId}/enrollments`;
-	return await get_(url, true, sessionId);
-};
